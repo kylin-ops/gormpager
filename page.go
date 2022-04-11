612bedfcd91f79fbec8bcafe914f090f0becdbbb
刘清 <liuqing2@shein.com>
2022-04-11 23:30:17 +0800

    init

---
+++ page.go
@@ -0,0 +1,194 @@
+package gormpager
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/kylin_ops/gormpager/query"
+	"gorm.io/gorm"
+)
+
+// 过滤参数
+type FilterArgs map[string]string
+
+
+// 过滤器
+type Options struct {
+	MaxPageSize        int
+	DefaultPageSize    int
+	PageSizeArgName    string
+	CurrentPageArgName string
+	OrderArgName       string
+	NoPageArgName      string
+}
+
+type filter struct {
+	Options
+}
+
+// 根据filter生成对于的查询参数
+func (o *filter)MakeNoPageQuery(filters FilterArgs) *query.Query {
+	var _querys []string
+	var _agrs []interface{}
+	var _order []query.OrderBy
+
+	for k, v := range filters {
+		if k == "order" {
+			_orders := strings.Split(v, ",")
+			for _, field := range _orders {
+				if field == "" {
+					continue
+				}
+				if field[:1] == "-" {
+					_order = append(_order, query.OrderBy{Field: field[1:], Order: -1})
+				} else {
+					_order = append(_order, query.OrderBy{Field: field, Order: 1})
+				}
+			}
+			continue
+		}
+
+		if k != "" {
+			_querys = append(_querys, k+" = ?")
+			_agrs = append(_agrs, v)
+		}
+	}
+	return &query.Query{
+		Where: strings.Join(_querys, " AND "),
+		Args:  _agrs,
+		Order: _order,
+	}
+}
+
+// 根据filter生成对于的查询参数
+func (o *filter)MakePageQuery(filters FilterArgs) *query.Query {
+	currentPage := 1
+	pageSize := o.DefaultPageSize
+	var _querys []string
+	var _agrs []interface{}
+	var _order []query.OrderBy
+	var _noPage bool
+
+	for k, v := range filters {
+		if k == o.CurrentPageArgName {
+			if _page, err := strconv.Atoi(v); err == nil {
+				currentPage = _page
+			}
+			continue
+		}
+		if k == o.PageSizeArgName {
+			if _size, err := strconv.Atoi(v); err == nil {
+				pageSize = _size
+				if pageSize > o.MaxPageSize {
+					pageSize = o.MaxPageSize
+				}
+			}
+			continue
+		}
+
+		if k == o.NoPageArgName {
+			_noPage = true
+			continue
+		}
+
+		if k == o.OrderArgName {
+			_orders := strings.Split(v, ",")
+			for _, field := range _orders {
+				if field == "" {
+					continue
+				}
+				if field[:1] == "-" {
+					_order = append(_order, query.OrderBy{Field: field[1:], Order: -1})
+				} else {
+					_order = append(_order, query.OrderBy{Field: field, Order: 1})
+				}
+			}
+			continue
+		}
+
+		if k != "" {
+			_querys = append(_querys, k+" = ?")
+			_agrs = append(_agrs, v)
+		}
+	}
+	return &query.Query{
+		Where:  strings.Join(_querys, " AND "),
+		Args:   _agrs,
+		Order:  _order,
+		Page:   currentPage,
+		Size:   pageSize,
+		NoPage: _noPage,
+		Limit:  pageSize,
+		Offset: (currentPage - 1) * pageSize,
+	}
+}
+
+// 分页列表查询器
+func (o *filter)PageQueryResult(db *gorm.DB, filters FilterArgs, results interface{}, preload ...string) (*query.Page, error) {
+	query := o.MakePageQuery(filters)
+	page, err := query.PageQuery(db)
+	if err != nil {
+		return nil, err
+	}
+	for _, p := range preload {
+		db = db.Preload(p)
+	}
+	if err := db.Find(results).Error; err != nil {
+		return nil, err
+	}
+	page.Results = results
+	return page, nil
+}
+
+// 不分页列表查询器
+func (o *filter)NoPageQueryResult(db *gorm.DB, filters FilterArgs, results interface{}, preload ...string) error {
+	query := o.MakePageQuery(filters)
+	db = query.Query(db)
+
+	for _, p := range preload {
+		db = db.Preload(p)
+	}
+	if err := db.Find(results).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// 自动选择器，通过判断filterArgs里是否有no_page的参数
+func (o *filter)QueryResult(db *gorm.DB,filters FilterArgs, results interface{}, preload ...string)(*query.Page, error) {
+	if _, ok := filters[o.NoPageArgName]; ok {
+		return o.PageQueryResult(db, filters, results, preload...)
+	}
+	err := o.NoPageQueryResult(db, filters, results, preload...)
+	return nil, err
+}
+
+
+
+func NewFilter(options *Options)*filter{
+	if options.MaxPageSize == 0 {
+		options.MaxPageSize = 50
+	}
+	if options.DefaultPageSize == 0 {
+		options.DefaultPageSize = 20
+	}
+	if options.DefaultPageSize > options.MaxPageSize {
+		options.DefaultPageSize = options.MaxPageSize
+	}
+	if options.PageSizeArgName == "" {
+		options.PageSizeArgName = "size"
+	}
+	if options.CurrentPageArgName == "" {
+		options.CurrentPageArgName = "page"
+	}
+	if options.OrderArgName == "" {
+		options.OrderArgName = "order"
+	}
+	if options.NoPageArgName == "" {
+		options.NoPageArgName = "no_page"
+	}
+
+	return &filter{
+		Options: *options,
+	}
+}
---
+++ query/query.go
@@ -0,0 +1,104 @@
+package query
+
+import (
+	"gorm.io/gorm"
+)
+
+type OrderBy struct {
+	Field string
+	Order int8 // 0不排序， >0正序； <0倒序
+}
+
+// 定义查询参数
+type Query struct {
+	Where  interface{}
+	Args   []interface{}
+	Order  []OrderBy
+	Limit  int
+	Offset int
+	Page   int
+	Size   int
+	NoPage bool
+}
+
+type Page struct {
+	// 数据总行数
+	TotalRow int `json:"total_row"`
+	// 总页数
+	TotalPage int `json:"total_page"`
+	// 没页多少数据
+	PageSize int `json:"page_size"`
+	// 当前页码
+	CurrentPage int `json:"current_page"`
+	// 返回的数据
+	Results interface{} `json:"results"`
+}
+
+
+func totalRow(db *gorm.DB) (int, error) {
+	var n int64
+	err := db.Count(&n).Error
+	return int(n), err
+}
+
+func totalPage(count, pageSize int) int {
+	page := count / pageSize
+	mod := count % pageSize
+	if mod > 0 {
+		page++
+	}
+	return page
+}
+
+// 返回查询拼接语句, gorm需要指定model
+func (q *Query) Query(d *gorm.DB) *gorm.DB {
+	if q.Where != nil {
+		d = d.Where(q.Where, q.Args...)
+	}
+
+	if q.Order != nil {
+		for _, order := range q.Order {
+			if order.Order > 0 {
+				d = d.Order(order.Field + " " + "ASC")
+			}
+			if order.Order < 0 {
+				d = d.Order(order.Field + " " + "DESC")
+			}
+		}
+	}
+	if q.Offset != 0 {
+		d = d.Offset(q.Offset)
+	}
+	if q.Limit == 0 {
+		q.Limit = 50
+	}
+	d = d.Limit(q.Limit)
+	return d
+}
+
+// 分页器, gorm需要指定model确定查下的表格，返回分页数据格式有基本数据
+func (q *Query) PageQuery(db *gorm.DB) (*Page, error) {
+	if q.Page < 1 {
+		q.Page = 1
+	}
+	db = q.Query(db)
+	row, err := totalRow(db)
+	if err != nil {
+		return nil, err
+	}
+	totalPage := totalPage(row, q.Size)
+
+	if q.Page > totalPage {
+		q.Page = totalPage
+	}
+	offset := (q.Page - 1) * q.Size
+	q.Limit = q.Size
+	q.Offset = offset
+	_page := &Page{
+		TotalRow:    row,
+		TotalPage:   totalPage,
+		PageSize:    q.Size,
+		CurrentPage: q.Page,
+	}
+	return _page, err
+}
